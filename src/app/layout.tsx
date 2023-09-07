fe691414c270d0312d00a7259dfe93e06df0ae26
xanderazuaje <charon-mortis@protonmail.com>
2023-09-07 12:53:37 +0200

    deleted default

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Calling App',
+  description: 'Something to do call',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
