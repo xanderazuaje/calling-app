c8510fdd76a9ffa3043470f712af045f915f1cec
xanderazuaje <charon-mortis@protonmail.com>
2023-09-07 12:53:45 +0200

    created webcam

---
+++ src/components/webcam.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useEffect, useRef } from "react";
+
+export default function Webcam() {
+    const videoRef = useRef<HTMLVideoElement>(null);
+    useEffect(() => {
+        const mediaDevices: MediaDevices = navigator.mediaDevices;
+        const video: HTMLVideoElement = videoRef.current!;
+        const constraints = {
+            video: true,
+        };
+        mediaDevices.getUserMedia(constraints)
+        .then((stream) => {
+            video!.srcObject = stream;
+        });
+    }, []);
+
+    return (
+        <div>
+            <video ref={videoRef} className="w-full h-full" autoPlay></video>
+        </div>
+    )
+}
